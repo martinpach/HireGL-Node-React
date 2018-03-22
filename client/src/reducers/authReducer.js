7125896863a61bb94f3649f1a0128168e0194626
Martin Pach <martin.pach@synapse.com>
2018-03-22 15:35:02 +0100

    Little refactor

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,6 @@
 import { LOGIN_ERROR, AUTH_USER, UNAUTH_USER } from '../actions/types';
 
 export default (state = {}, action) => {
-    console.log(state);
     switch (action.type) {
         case AUTH_USER:
             return { isAuthenticated: true };
