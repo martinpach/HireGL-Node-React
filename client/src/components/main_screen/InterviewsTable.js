42f6e72e5c22c132f109bf9b263552f800ce6bf8
Martin Pach <martin.pach@synapse.com>
2018-03-27 09:23:31 +0200

    Added candidate position into table

--- client/src/components/main_screen/InterviewsTable.js
+++ client/src/components/main_screen/InterviewsTable.js
@@ -4,7 +4,8 @@ import Spinner from 'react-md-spinner';
 const InterviewRow = interview => {
     return (
         <tr key={interview._id}>
-            <td>{interview.candidate.firstName} {interview.candidate.lastName}</td>
+            <td>{interview.candidate.firstName} {interview.candidate.lastName} <br />
+                {interview.candidate.position}</td>
             <td>{interview.candidate.phone}</td>
             <td>{interview.candidate.email}</td>
             <td>{interview.interview.status}</td>
